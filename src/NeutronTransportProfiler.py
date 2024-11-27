7db1189541734aa3f4eae83d98164398910923de
Befries <adejolier@outlook.be>
2024-11-27 13:26:42 +0100

    feat: add efficiency evaluation

--- src/NeutronTransportProfiler.py
+++ src/NeutronTransportProfiler.py
@@ -6,22 +6,28 @@ import time
 from NeutronTransport import simulate_transport
 
 
-thickness_tests = 5
+thickness_tests = 10
 thicknesses = np.linspace(0.01, 0.1, thickness_tests)
+pop_size = int(1e5)
 penetrations = np.empty(thickness_tests)
 variances = np.empty(thickness_tests)
+simulation_time = np.empty(thickness_tests)
 
-start = time.perf_counter()
+
 for i, thickness in enumerate(thicknesses):
-    penetrations[i], variances[i] = simulate_transport(500, 200, thickness, int(1e5))
-elapsed = time.perf_counter() - start
+    start = time.perf_counter()
+    penetrations[i], variances[i] = simulate_transport(500, 200, thickness, pop_size)
+    simulation_time[i] = time.perf_counter() - start
 
-print("simulation run in", elapsed, "seconds")
+total_time = np.sum(simulation_time)
+Efficiency = 1 / (variances * simulation_time) * pop_size
+
+print("all simulation run in", total_time, "seconds")
 
 fig, axs = plt.subplots(2)
 axs[0].scatter(thicknesses, penetrations)
 axs[0].set_title("penetration probability")
-axs[1].scatter(thicknesses, variances)
+axs[1].scatter(thicknesses, Efficiency)
 axs[1].set_title("variance estimation")
 
 for ax in axs:
