4d79c4e75798640e2cb019b76308dc94c6d75155
Befries <adejolier@outlook.be>
2024-11-28 20:44:57 +0100

    refactor: change paradigm pf evaluation, slows down the program but enables easier consideration of splitting

--- src/NeutronTransport.py
+++ src/NeutronTransport.py
@@ -8,8 +8,23 @@ class NeutronPopulation:
         self.cosines = np.ones(pop_size)
         self.weights = np.ones(pop_size)
 
+        self.contributions = np.zeros(pop_size)
+        # start of each neutron descendant in the population
+        self.descendant_indices = np.arange(0, pop_size, dtype=int)
+
     def size(self) -> int:
         return self.positions.size
+
+    def clear_dead_neutron(self, living_criteria):
+        has_legacy = np.diff(self.descendant_indices, append=self.positions.size) > 0
+        cumulative_sizes = np.cumsum(np.add.reduceat(
+            living_criteria.astype(int),
+            self.descendant_indices, dtype=int) * has_legacy)
+        self.descendant_indices = np.insert(cumulative_sizes[:-1], 0, 0)
+
+        self.positions = self.positions[living_criteria]
+        self.weights = self.weights[living_criteria]
+        self.descendant_indices[self.descendant_indices == self.positions.size] -= 1
 
 
 def free_flight_sampling(proba_per_ul, cos_theta, sample_size) -> np.ndarray:
@@ -48,38 +63,43 @@ def free_flight(n_population: NeutronPopulation, wall_thickness, sigma_t) -> (fl
 
     n_population.positions = np.sign(n_population.cosines) * free_flight_distance + n_population.positions
     alive = n_population.positions <= wall_thickness
-    n_population.positions = n_population.positions[alive]
 
-    # change that part to account for better estimator, take weights into account
-    sub_estimator = initial_pop_size - np.size(n_population.positions)
-    sub_var_estimator = sub_estimator  # for the moment (1^2 = 1 so don't need to do fancy stuff for now)
-    return sub_estimator, sub_var_estimator
+    # every prime neutron that still has descendants
+    has_legacy = np.diff(n_population.descendant_indices, append=n_population.size()) > 0
+
+    # had the contribution of the descendants to their ancestor score
+    # print(n_population.descendant_indices)
+    sub_contrib = np.add.reduceat(
+        n_population.weights * ~alive,
+        n_population.descendant_indices
+    )
+    n_population.contributions[has_legacy] += sub_contrib[has_legacy]
+
+    # no neutron actually dies here in a free flight estimation, but need to put it for the basic algorithm
+    n_population.clear_dead_neutron(alive)
 
 
 def simulate_transport(sigma_a, sigma_s, thickness, sample_size):
     sigma_t = sigma_s + sigma_a
     p_absorption = sigma_a / sigma_t
-
-    estimator = 0
-    var_estimator = 0
-
     neutron_population = NeutronPopulation(sample_size)
 
     while neutron_population.size() > 0:
 
         # free flight:
-        current_estimator, current_var_estimator = free_flight(neutron_population, thickness, sigma_t)
-        estimator += current_estimator  # I += I_i
-        var_estimator += current_var_estimator - current_estimator**2 / sample_size  # D^2(I) += D^2(I_i)
+        free_flight(neutron_population, thickness, sigma_t)
+        if neutron_population.size() <= 0:
+            break
 
         # better estimator for scattering & absorption + russian roulette?
         not_dead = (neutron_population.positions >= 0) | interaction_sampling(p_absorption, neutron_population.size())
-        neutron_population.positions = neutron_population.positions[not_dead]
+        neutron_population.clear_dead_neutron(not_dead)
 
         # splitting strategies
 
         # biasing strategies
         neutron_population.cosines = np.cos(np.random.uniform(low=0, high=np.pi, size=neutron_population.size()))
 
-    return estimator / sample_size, var_estimator / sample_size
-
+    estimator = np.sum(neutron_population.contributions) / sample_size
+    var_estimator = np.sum(neutron_population.contributions**2) / sample_size - estimator**2
+    return estimator, var_estimator
