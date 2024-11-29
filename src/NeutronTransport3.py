a045119e2479dcf5270f4c2e6a95bec88fb724db
Befries <adejolier@outlook.be>
2024-11-30 00:52:42 +0100

    feat: Code of the 3rd version

---
+++ src/NeutronTransport3.py
@@ -0,0 +1,68 @@
+import numpy as np
+
+
+def free_flight_sampling(positions, director_cosine, proba_per_ul, rest_estimation):
+    sample = np.random.uniform(size=positions.size)
+    return - np.log(1 - rest_estimation * sample) * director_cosine / proba_per_ul
+
+
+def free_flight_estimator(positions, director_cosines, sigma_total, wall_thickness):
+    return np.where(director_cosines > 0,
+                    np.exp(-sigma_total * (wall_thickness - positions)/director_cosines),
+                    np.exp(sigma_total * positions / director_cosines)
+                    )
+
+
+def split(positions, weights, m):
+    return np.repeat(positions, m), np.repeat(weights / m, m)
+
+
+def russian_roulette(weights, threshold):
+    sample = np.random.uniform(size=weights.size)
+    gun_loaded = weights < threshold
+    trigger_safe = sample < threshold
+
+    survivor = gun_loaded & trigger_safe
+    weights[survivor] = weights[survivor] / threshold
+    return ~gun_loaded | trigger_safe
+
+
+def simulate_transport(capture_scattering_ratio, sigma_total, wall_thickness, population_size, split_factor, threshold):
+    scatter_probability = 1 / (capture_scattering_ratio + 1)
+    split_frequency = 4
+
+    estimator = 0
+    second_estimator = 0  # estimator of the sum of square contributions
+    for i in range(population_size):
+        # initial values for the incident beam
+        positions = np.zeros(1)
+        weights = np.ones(1)
+        director_cosine = np.ones(1)
+
+        contribution = 0.0
+        collision = 0
+
+        while positions.size > 0:
+
+            free_flight_estimate = free_flight_estimator(positions, director_cosine, sigma_total, wall_thickness)
+            contribution += np.sum(weights * free_flight_estimate, where=director_cosine > 0)
+            rest_estimation = 1 - free_flight_estimate
+            weights *= scatter_probability * rest_estimation
+
+            positions += free_flight_sampling(positions, director_cosine, sigma_total, rest_estimation)
+
+            if collision % split_frequency == 0:
+                positions, weights = split(positions, weights, split_factor)
+            if (collision - 1) % split_frequency == 0:
+                not_fucking_dead = russian_roulette(weights, threshold)
+                positions = positions[not_fucking_dead]
+                weights = weights[not_fucking_dead]
+
+            collision += 1
+            director_cosine = np.cos(np.random.uniform(low=0.0, high=np.pi, size=positions.size))
+
+        estimator += contribution
+        second_estimator += contribution ** 2
+    estimator = estimator / population_size
+    variance = second_estimator / population_size - estimator ** 2  # it's the value for a simple binary counter
+    return estimator, variance
--- src/NeutronTransportProfiler.py
+++ src/NeutronTransportProfiler.py
@@ -2,13 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
-from NeutronTransport2 import simulate_transport
+from NeutronTransport3 import simulate_transport
 
 
 def test_thicknesses():
     thickness_tests = 8
     thicknesses = np.linspace(2, 10, thickness_tests)
-    pop_size = int(1e4)
+    pop_size = int(1e3)
     penetrations = np.empty(thickness_tests)
     variances = np.empty(thickness_tests)
     simulation_time = np.empty(thickness_tests)
@@ -20,8 +20,8 @@ def test_thicknesses():
             0.8,
             thickness,
             pop_size,
-            15,
-            0.003
+            20,
+            0.0001
         )
         simulation_time[i] = time.perf_counter() - start
 
@@ -39,7 +39,7 @@ def test_thicknesses():
 
     color = 'tab:red'
     axs[1].plot(thicknesses, efficiency, color=color)
-    axs[1].set_title("Efficiency and Standard Deviation")
+    axs[1].set_title("Efficiency and Variance")
     axs[1].set_ylabel("Efficiency", color=color)
     axs[1].tick_params(axis='y', labelcolor=color)
     axs[1].set_xlabel("thickness [cm]")
@@ -47,9 +47,9 @@ def test_thicknesses():
 
     color = 'tab:blue'
     var_ax = axs[1].twinx()
-    var_ax.scatter(thicknesses, np.sqrt(variances / pop_size), color=color)
+    var_ax.scatter(thicknesses, variances, color=color)
     var_ax.set_yscale('log')
-    var_ax.set_ylabel("standard deviation", color=color)
+    var_ax.set_ylabel("Variance s²", color=color)
     var_ax.tick_params(axis='y', labelcolor=color)
     var_ax.grid(visible=True, which='both')
 
@@ -58,23 +58,22 @@ def test_thicknesses():
 
 
 def test_split():
-    pop_size = int(1e4)
+    pop_size = int(1e3)
     split_range_size = 10
-    step_size = 3
-    split_range = np.arange(1, 1 + split_range_size * step_size, step_size)
+    threshold_range = np.linspace(0.001, 0.01, split_range_size)
     estimations = np.empty(split_range_size)
     variances = np.empty(split_range_size)
     simulation_time = np.empty(split_range_size)
 
-    for i, split in enumerate(split_range):
+    for i, split in enumerate(threshold_range):
         start = time.perf_counter()
         estimations[i], variances[i] = simulate_transport(
             0.3,
             0.8,
             10,
             pop_size,
-            split,
-            0.003
+            15,
+            split
         )
         simulation_time[i] = time.perf_counter() - start
 
@@ -83,14 +82,14 @@ def test_split():
 
     print("all simulation run in", total_time, "seconds")
     fig, axs = plt.subplots(2)
-    axs[0].scatter(split_range, variances)
+    axs[0].scatter(threshold_range, variances)
     axs[0].set_title("variances")
     axs[0].set_yscale('log')
     axs[0].set_xlabel("split amount")
     axs[0].grid(visible=True, which='both', axis='both')
 
     color = 'tab:red'
-    axs[1].plot(split_range, efficiency, color=color)
+    axs[1].plot(threshold_range, efficiency, color=color)
     axs[1].set_title("Efficiency")
     axs[1].set_ylabel("Efficiency", color=color)
     axs[1].tick_params(axis='y', labelcolor=color)
@@ -101,4 +100,4 @@ def test_split():
     plt.show()
 
 
-test_split()
+test_thicknesses()
