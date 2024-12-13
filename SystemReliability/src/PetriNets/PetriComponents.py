f066c68b8b9ae6ff560e7ac09a35186360f10873
Befries <adejolier@outlook.be>
2024-12-13 09:59:35 +0100

    feat: almost completed petri net logic and components

--- SystemReliability/src/PetriNets/PetriComponents.py
+++ SystemReliability/src/PetriNets/PetriComponents.py
@@ -1,8 +1,10 @@
+import random
+import numpy as np
 
 class Message:
     def __init__(self, name: str):
         self.name = name
-        self.activated = False
+        self.value = False
 
 
 class Place:
@@ -29,31 +31,47 @@ class Transition:
     inhibitors: list[Place]
     inhibitor_weights: list[int]
 
-    received_messages: list[Message]
-    sent_messages: list[Message]
+    observed_message: list[tuple[Message, bool]]  # list of message to listen to and the expected value for the firing
+    broadcast_messages: list[tuple[Message, bool]]  # list of message to change and the value to give them
 
     def __init__(self, name: str):
         self.name = name
+        self.stochastic = []  # != None only if the transition is stochastic
 
     def add_upstream(self, upstream_place: Place, weight=1):
         self.upstream_places.append(upstream_place)
         self.upstream_weights.append(weight)
 
     def add_downstream(self, downstream_place: Place, weight=1):
+        if len(self.stochastic) > 0:
+            raise Exception("Stochastic transition, cannot add simple sub-places")
         self.downstream_places.append(downstream_place)
         self.downstream_weights.append(weight)
+
+    def add_downstream_stochastic(self, downstream_place: Place, transition_probability: float, weight=1):
+        if len(self.downstream_places) > len(self.stochastic):
+            raise Exception("Deterministic transition, cannot add stochastic sub-places")
+        self.add_downstream(downstream_place, weight)
+        self.stochastic.append(transition_probability)
+
 
     def add_inhibitor(self, inhibitor_place: Place, weight=1):
         self.inhibitors.append(inhibitor_place)
         self.inhibitor_weights.append(weight)
+
+    def attach_message(self, message: Message, expected_value: bool):
+        self.observed_message.append((message, expected_value))
+
+    def attach_emitter(self, message: Message, value: bool):
+        self.broadcast_messages.append((message, value))
 
     def check_armed_state(self):
         """
         checks if all messages are True, inhibitors inactivated and that upstream places contain enough tokens
         :return: whether the transition is armed and can be traversed
         """
-        for message in self.received_messages:
-            if not message:
+        for message, expected_message in self.observed_message:
+            if message is not expected_message:
                 return False
         for inhibitor_place, inhibitor_weight in zip(self.inhibitors, self.inhibitor_weights):
             if inhibitor_place.token >= inhibitor_weight:
@@ -63,13 +81,43 @@ class Transition:
                 return False
         return True
 
+    def sample_firing(self):
+        pass  # overridden in child classes
+
+    def pass_tokens(self):
+        for message, broadcast in self.broadcast_messages:
+            message.value = broadcast
+
+        for upstream_place, upstream_weight in zip(self.upstream_places, self.upstream_weights):
+            upstream_place.token -= upstream_weight
+
+        if len(self.stochastic) > 0:
+            random.choices(list(zip(self.downstream_places, self.downstream_weights)), weights=self.stochastic)
+            return
+
+        for down_stream_place, downstream_weight in zip(self.downstream_places, self.downstream_weights):
+            down_stream_place.token += downstream_weight
+
+
 
 class InstantTransition(Transition):
     def __init__(self, name: str):
         super().__init__(name)
+
+    def sample_firing(self) -> float:
+        """
+        :return: fires instantly
+        """
+        return 0.0
 
 
 class SlowTransition(Transition):
     def __init__(self, name: str, transition_rate: float):
         super().__init__(name)
         self.transition_rate = transition_rate
+
+    def sample_firing(self) -> float:
+        """
+        :return: the time after which the transition is fired
+        """
+        return -1.0/self.transition_rate * np.log(np.random.uniform())
--- SystemReliability/src/PetriNets/PetriNetSimulation.py
+++ SystemReliability/src/PetriNets/PetriNetSimulation.py
@@ -1,6 +1,5 @@
 from PetriComponents import *
 
-
 class PetriNetSystem:
 
     places: list[Place]
@@ -17,7 +16,7 @@ class PetriNetSystem:
 
     def __sort__(self):
         """
-        puts all the InstantTransition are at the start of the list, so they are checked first
+        puts all the InstantTransition at the start of the list, so they are checked first
         """
         self.transitions.sort(key=lambda a: type(a) is not InstantTransition)
 
@@ -37,8 +36,26 @@ class PetriNetSystem:
             place.reset_tokens()
 
         lifetime = 0.0
-
         # should update this condition -> if all transitions unarmed -> impossible to continue, therefore end of journey
         while lifetime < duration:
+            transition_fired, time_passed = self.choose_candidate(duration, lifetime)
+            lifetime += time_passed
+            if transition_fired is None:  # time exceeded or complete fail state
+                # add info based on end situation
+                break
+
+            transition_fired.pass_tokens()
             # life cycle of the net
-            pass
+
+    def choose_candidate(self, duration, lifetime) -> tuple[Transition, float]:
+        candidate = None
+        candidate_transition_time = duration - lifetime  # max transition time
+        for transition in self.transitions:
+            if transition.check_armed_state():
+                candidate_time = transition.sample_firing()
+                if candidate_time is 0.0:  # instant transition
+                    return transition, 0.0
+                if candidate_time < candidate_transition_time:
+                    candidate_transition_time = candidate_time
+                    candidate = transition
+        return candidate, candidate_transition_time
